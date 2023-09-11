54505d834fb2646fc2a3ebfc27ebf2c65a194318
Gopal <gopaldeshkari@gmail.com>
2023-09-12 05:17:41 +0530

    first dart program

---
+++ 2datatype.dart
@@ -0,0 +1,24 @@
+void main() {
+  int age = 10;
+var score = 50;
+int lacs = 0xADFEAAEEA;
+
+
+  print(age);
+  print(score);
+  print(lacs);
+
+String name = "henry";
+var company = "google";
+
+print(name);
+print(company);
+
+double percent=94.3;
+var percentage = 94.3;
+double exp  = 1.25e41;
+
+print(percent);
+print(percentage);
+print(exp);
+}

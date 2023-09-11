78e35bfec752a5922cc73b845a50138e63ebda32
Gopal <gopaldeshkari@gmail.com>
2023-09-12 04:57:50 +0530

    first dart program

---
+++ 1first.dart
@@ -0,0 +1,11 @@
+void main() {
+  // this first line of program
+  print("hello world");
+
+  print("this is my first code"); // This is another comment
+
+  print(4-1);
+
+  print(true); //This is boolean value
+}
+

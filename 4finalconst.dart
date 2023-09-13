8c8d414b0072c36cab779c4824b2bdc0d9791272
Gopal <gopaldeshkari@gmail.com>
2023-09-14 04:18:49 +0530

    914

--- 4finalconst.dart
+++ 4finalconst.dart
@@ -1,3 +1,18 @@
 void main(){
 
+  //final keyword
+  final cityname = "Mumbai";
+  //cityname ='Delhi';
+  final String Countryname = 'india';
+
+  //const keyword
+  const PI = 3.14;
+  const double gravity =9.8;
+
+}
+
+class Circle{
+  final color ='Red';
+  static const PI =3.14;
+
 }
--- 5controlflow.dart
+++ 5controlflow.dart
@@ -1,1 +1,32 @@
-void main(
+void main(){
+  //If and Else Statement
+  var salary =15000;
+  if(salary>20000){
+    print("you got promotion,Congratulations");}
+  else{
+    print("you need to work hard");}
+
+
+  // IF ELSE IF Statement
+  var marks = 70;
+  if (marks>=90 && marks<100){
+    print("A+ grade");
+  }
+  else if(marks>=80 && marks <90){
+    print("A grade" );
+  }
+  else if(marks>=70 && marks <80){
+    print("B grade" );
+  }
+  else if(marks>=60 && marks <70){
+    print("C grade" );
+  }
+  else if( marks <30){
+    print("You have Failed" );
+  }
+  else {
+    print("Invalid Marks,Please Try");
+  }
+
+
+}

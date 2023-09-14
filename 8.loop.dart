264ac06f8bbef51e257b9f874bc14f3b688a45d0
Gopal <gopaldeshkari@gmail.com>
2023-09-14 06:04:54 +0530

    914

---
+++ 8.loop.dart
@@ -0,0 +1,25 @@
+void main() {
+//For Loop
+
+  for (int i = 1; i <= 1000000; i++) {
+    if (i % 2 == 0) print(i);
+  }
+}
+
+
+// FOR LOOP:
+// for(var i=0;i<4;i++){
+// print("hello")
+// }
+
+// WHILE LOOP;
+// while (i<4){
+//   print("hello")
+//   I++;
+// }
+
+//DO WHILE LOOP
+// do { 
+//   print("helllo")
+//   i++;
+// } while(i<4);

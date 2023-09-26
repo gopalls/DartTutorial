62fa55b5dfd25bc55e5ed277005b5a8912bbe44c
Gopal <gopaldeshkari@gmail.com>
2023-09-26 15:05:39 +0530

    9/26

--- Function_Method/optionalMethod.dart
+++ Function_Method/optionalMethod.dart
@@ -1,9 +1,18 @@
 //Required parameter
 //Optional Parameter
+//Optional Named parameters
+//Optional Default parameters
 void main() {
   printCities("Newyork", "Delhi", "Mumbai");
 
+  print(" ");
   printCountry("India", "USA");
+
+  print(" ");
+  findVolume(10, height: 5, breadth: 10);
+
+  print(" ");
+  findArea(10);
 }
 
 //Required Parameters
@@ -15,8 +24,25 @@ void printCities(String Name1, String Name2, String Name3) {
 
 //Optional Parameters
 
-void printCountry(String Name1, String Name2, [var  Name3]) {
+void printCountry(String Name1, String Name2, [var Name3]) {
   print("Name 1 is $Name1");
   print("Name 2 is $Name2");
   print("Name 3 is $Name3");
+}
+
+//Optional Named parameters
+void findVolume(int length, {var breadth, var height}) {
+  print("Length is $length");
+  print("breadth is $length");
+  print("Height is $height");
+
+  print("volume is ${length * breadth * height}");
+}
+
+//Optional Default parameters
+void findArea(int length, {var breadth = 20}) {
+  print("Length is $length");
+  print("breadth is $length");
+
+  print("volume is ${length * breadth}");
 }
